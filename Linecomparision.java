0ae0ab5424995938b6183b285af9e687172b9d3d
mrinal kaushik <mrinalrocks24x7gmail.com>
2022-11-18 11:13:58 +0530

    ADD: length of line 1 and line 2

--- Linecomparision.java
+++ Linecomparision.java
@@ -5,10 +5,36 @@ import java.util.Scanner;
 /* start display*/
 
 public class Linecomparision {
+	/* measuring length of line 1 and line 2 */
+	public static void line() {
+		Scanner num = new Scanner(System.in);
+		System.out.println("enter value of x1:");
+		int x1 = num.nextInt();
+		System.out.println("enter value of x2:");
+		int x2 = num.nextInt();
+		System.out.println("enter value of y1:");
+		int y1 = num.nextInt();
+		System.out.println("enter value of y2:");
+		int y2 = num.nextInt();
+		System.out.println("enter value of x3 :");
+		int x3 = num.nextInt();
+		System.out.println("enter value of x4:");
+		int x4 = num.nextInt();
+		System.out.println("enter value of y3:");
+		int y3 = num.nextInt();
+		System.out.println("enter value of y4:");
+		int y4 = num.nextInt();
+		double z1 = Math.floor(Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2));
+		System.out.println("length of line 1=" + z1);
+		double z2 = Math.floor(Math.sqrt((x4 - x3) ^ 2 + (y4 - y3) ^ 2));
+		System.out.println("length of line 2 = " + z2);
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line\r\n" + "Comparison Computation\r\n" + "Program on Master Branch");
 		;
+		Linecomparision.line();
+		
 	}
 
 }
