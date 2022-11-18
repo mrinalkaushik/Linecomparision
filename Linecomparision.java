aa21f25be072390ac4c4814f0abd1887ae6723fe
mrinal kaushik <mrinalrocks24x7gmail.com>
2022-11-18 11:27:33 +0530

    ADD: comparing line 1 and line 2

--- Linecomparision.java
+++ Linecomparision.java
@@ -5,8 +5,9 @@ import java.util.Scanner;
 /* start display*/
 
 public class Linecomparision {
-	/* measuring length of line 1 and line 2 */
-	public static void line() {
+	/* measuring length of line 1 and line 2  and comparing they are equal or not */
+
+	public static void linecompare() {
 		Scanner num = new Scanner(System.in);
 		System.out.println("enter value of x1:");
 		int x1 = num.nextInt();
@@ -28,13 +29,19 @@ public class Linecomparision {
 		System.out.println("length of line 1=" + z1);
 		double z2 = Math.floor(Math.sqrt((x4 - x3) ^ 2 + (y4 - y3) ^ 2));
 		System.out.println("length of line 2 = " + z2);
+
+		/* lines are equal or not */
+		if (z1 == z2) {
+			System.out.println("length of both lines are equal");
+		} else {
+			System.out.println("length of both lines are not equal");
+		}
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line\r\n" + "Comparison Computation\r\n" + "Program on Master Branch");
 		;
-		Linecomparision.line();
-		
+		Linecomparision.linecompare();
 	}
 
 }
