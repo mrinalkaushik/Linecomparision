e03ad21320dd7f89ee0789c92ac976c0204ef79b
mrinal kaushik <mrinalrocks24x7gmail.com>
2022-11-18 11:06:39 +0530

    ADD:welcome to line comparision program

---
+++ Linecomparision.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.linecomparision;
+
+import java.util.Scanner;
+
+/* start display*/
+
+public class Linecomparision {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line\r\n" + "Comparison Computation\r\n" + "Program on Master Branch");
+		;
+	}
+
+}
